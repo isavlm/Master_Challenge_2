6a82582fc40d711626d4173cc483bcbb2fcd014d
Isavlima <ivlm@pm.me>
2024-09-30 09:36:08 -0700

    Created and edited files for the edit endpoint.

---
+++ app/src/use_cases/product/edit/use_case.py
@@ -0,0 +1,34 @@
+from typing import Optional
+
+from app.src.exceptions import ProductNotFoundException, ProductRepositoryException
+
+from app.src.core.models import Product
+from app.src.repositories import ProductRepository
+
+from .request import EditProductRequest
+from .response import EditProductResponse
+
+
+class EditProduct:
+    def __init__(self, product_repository: ProductRepository) -> None:
+        self.product_repository = product_repository
+
+    def __verify_product_exists(
+        self, product: Optional[Product], request_entity_id: str
+    ) -> None:
+        if product is None:
+            raise ProductNotFoundException(product_id=request_entity_id)
+
+    def __call__(self, product_id: str, request: EditProductRequest) -> EditProductResponse:
+        try:
+            existing_product = self.product_repository.get_by_id(product_id)
+            print(existing_product)
+            self.__verify_product_exists(
+                existing_product, request_entity_id=request.product_id
+            )
+            response=self.product_repository.edit(request)
+            
+            return response
+        except ProductRepositoryException as e:
+            raise e 
+        
